c8400e10e16f2f5fd9589972139cc149e6c794ae
Jack Cherng <jfcherng@gmail.com>
2021-06-14 15:36:35 +0800

    chore: bump version 4107
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- sublime_plugin.pyi
+++ sublime_plugin.pyi
@@ -1,6 +1,6 @@
 # This file is maintained on https://github.com/jfcherng-sublime/ST-API-stubs
 #
-# ST version: 4100
+# ST version: 4107
 
 from importlib.machinery import ModuleSpec
 from types import ModuleType
